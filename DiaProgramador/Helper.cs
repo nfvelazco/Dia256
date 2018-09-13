a3bb8eb0ecfea764cf82227d114f42d6835bdff3
NiCo Velazco <nfvelazco@gmail.com>
2018-09-13 13:52:48 -0300

    first commit

---
+++ DiaProgramador/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaProgramador
+{
+    public static class Personal
+    {
+        public const string Secretaria = "Secretaria"; 
+        public const string GDP = "GDP"; 
+        public const string Mujer = "Mujer";
+        public const string Programador = "Programador"; 
+
+    }
+
+    public class DiasFestivos {
+        static new Dictionary<DateTime, string> geter = new Dictionary<DateTime, string>();
+
+        public static Dictionary<DateTime, string> Get {
+            get {
+                geter.Add(new DateTime(2018, 00, 00), Personal.Secretaria);
+                geter.Add(new DateTime(2018, 00, 00), Personal.Mujer);
+                geter.Add(new DateTime(2018, 00, 00), Personal.GDP);
+                geter.Add(new DateTime(2018, 00, 00), Personal.Programador);
+                return geter;
+            }
+        }
+                
+    }
+}
---
+++ DiaProgramador/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaProgramador
+{
+    class Program
+    {
+        public class Gift {
+
+        }
+        public class Issue {
+            public bool Finished { get; set; }
+        }
+        public static class Secretarias {
+            public static Gift Send(Gift Regalito) {
+                return Regalito;
+            }
+        }
+        public static class Mujeres
+        {
+            public static Gift Send(Gift Regalito)
+            {
+                return Regalito;
+            }
+        }
+        public static class GDPs
+        {
+            public static Gift Send(Gift Regalito)
+            {
+                return Regalito;
+            }
+        }
+        public static class Programadores {
+            public static Issue Send(Issue Regalito) {
+                return Regalito;
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            var diasFestivos = DiasFestivos.Get;
+            foreach (var dia in diasFestivos)
+            {
+                string format = "Feliz día {0}, aqui va tu premio!";
+
+                switch (dia.Value)
+                {
+                    case Personal.Secretaria:
+                        Console.Write(string.Format(format, Personal.Secretaria));
+                        Secretarias.Send(new Gift());
+                        break;
+                    case Personal.GDP:
+                        Console.Write(string.Format(format, Personal.GDP));
+                        GDPs.Send(new Gift());
+                        break;
+                    case Personal.Mujer:
+                        Console.Write(string.Format(format, Personal.Mujer));
+                        Mujeres.Send(new Gift());
+                        break;
+                    case Personal.Programador:
+                        Console.Write(string.Format(format, Personal.Programador));
+                        Programadores.Send(new Issue());
+                        break;
+                }
+            }
+        }
+    }
+}
